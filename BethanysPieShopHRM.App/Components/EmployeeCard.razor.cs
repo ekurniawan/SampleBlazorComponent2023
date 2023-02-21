8250480e1e8a9e9b69c28247996c9873facc6a1a
Erick Kurniawan <erick.kurniawan@gmail.com>
2023-02-21 10:25:15 +0700

    Add project files.

---
+++ BethanysPieShopHRM.App/Components/EmployeeCard.razor.cs
@@ -0,0 +1,9 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShopHRM.App.Components
+{
+    public partial class EmployeeCard
+    {
+        public Employee Employee { get; set; } = default!;
+    }
+}
