39d3047b14a44c8921af7b0636e1ce74bb3d6d11
Erick Kurniawan <erick.kurniawan@gmail.com>
2023-02-21 11:04:02 +0700

    memanbahkan quick popup component

---
+++ BethanysPieShopHRM.App/Components/QuickViewPopup.razor.cs
@@ -0,0 +1,13 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.App.Components
+{
+    public partial class QuickViewPopup
+    {
+        private Employee? _employee;
+
+        [Parameter]
+        public Employee? Employee { get; set; }
+    }
+}
