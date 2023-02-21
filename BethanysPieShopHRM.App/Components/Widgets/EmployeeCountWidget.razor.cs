705255096e0cf76d1bd116fbea63036e50eecd59
Erick Kurniawan <erick.kurniawan@gmail.com>
2023-02-21 14:15:05 +0700

    menambahkan dynamic component

---
+++ BethanysPieShopHRM.App/Components/Widgets/EmployeeCountWidget.razor.cs
@@ -0,0 +1,13 @@
+﻿using BethanysPieShopHRM.App.Models;
+
+namespace BethanysPieShopHRM.App.Components.Widgets
+{
+    public partial class EmployeeCountWidget
+    {
+        public int EmployeeCounter { get; set; }
+        protected override void OnInitialized()
+        {
+            EmployeeCounter = MockDataService.Employees.Count;
+        }
+    }
+}
