c7838ff928a667e7bfd700bd70b690669a8d7cb3
Erick Kurniawan <erick.kurniawan@gmail.com>
2023-02-22 10:54:11 +0700

    menambahkan services

--- BethanysPieShopHRM.App/Program.cs
+++ BethanysPieShopHRM.App/Program.cs
@@ -1,4 +1,5 @@
 using BethanysPieShopHRM.App;
+using BethanysPieShopHRM.App.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -7,6 +8,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
+
+builder.Services.AddHttpClient<IEmployeeDataService,EmployeeDataService>(client=>
+client.BaseAddress=new Uri(builder.HostEnvironment.BaseAddress));
+
 
 
 await builder.Build().RunAsync();
---
+++ BethanysPieShopHRM.App/Services/IEmployeeDataService.cs
@@ -0,0 +1,11 @@
+﻿namespace BethanysPieShopHRM.App.Services
+{
+    public interface IEmployeeDataService
+    {
+        Task<IEnumerable<Employee>> GetAllEmployees(bool refreshRequired = false);
+        Task<Employee> GetEmployeeDetails(int employeeId);
+        Task<Employee> AddEmployee(Employee employee);
+        Task UpdateEmployee(Employee employee);
+        Task DeleteEmployee(int employeeId);
+    }
+}
